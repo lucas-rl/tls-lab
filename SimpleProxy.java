75a9c98b7f5496490b671b2b627171388f810f4c
didier-leticia <leticia.francisco.61006@ga.ita.br>
2026-04-07 17:56:02 -0300

    day 8 - finished

--- SimpleProxy.java
+++ SimpleProxy.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -30,56 +27,33 @@ public class SimpleProxy {
         try{
             System.out.println("\n=== New Proxy Connection ===");
 
-            BufferedReader clientReader = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream())
-            );
-            //Connect to real server
+            InputStream clientInputStream = clientSocket.getInputStream();
+            OutputStream clientOutputStream = clientSocket.getOutputStream();
+
             Socket serverSocket = new Socket("localhost", 8080);
-            PrintWriter serverWriter = new PrintWriter(serverSocket.getOutputStream());
-            //Forward request line + headers
-            String line;
-            int contentLength = 0;
-            while ((line = clientReader.readLine()) != null && !line.isEmpty()) {
-                System.out.println("CLIENT -> " + line);
-                serverWriter.print(line + "\r\n");
-                if (line.toLowerCase().startsWith("content-length:")) {
-                    contentLength = Integer.parseInt(line.split(":")[1].trim());
-                }
-            }
-            serverWriter.print("\r\n");
-
-            //Read and foward body
-            char[] bodyChars = new char[contentLength];
-            if (contentLength > 0) {
-                clientReader.read(bodyChars, 0, contentLength);
-                String body = new String(bodyChars);
-                System.out.println("CLIENT BODY -> " + body);
-
-                // Forward body (we will modify next)
-                serverWriter.print(body);
-            }
-
-            serverWriter.flush();
+            InputStream serverInputStream = serverSocket.getInputStream();
+            OutputStream serverOutputStream = serverSocket.getOutputStream();
 
 
-            //Foward back response back to client
-            BufferedReader serverReader = new BufferedReader(
-                    new InputStreamReader(serverSocket.getInputStream())
-            );
-            PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream());
-            line = null;
-            while ((line = serverReader.readLine()) !=null && !line.isEmpty()){
-                System.out.println("SERVER -> " + line);
-                clientWriter.println(line);
-            }
-            clientWriter.flush();
-
-            //Close connections
-            serverSocket.close();
-            clientSocket.close();
+            new Thread(() -> forward(clientInputStream, serverOutputStream)).start();
+            new Thread(() -> forward(serverInputStream, clientOutputStream)).start();
 
         } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    private static void forward(InputStream in, OutputStream out) {
+        try {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+                out.flush();
+            }
+        } catch (IOException e) {
+            // connection closed
         }
     }
 }
